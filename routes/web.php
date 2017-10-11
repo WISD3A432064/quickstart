21ef222debb9988b33bce2238c1f76e3f3da4cc8
nini <poikl15397@gmail.com>
2017-10-11 18:19:00 +0800

    '新增任務'

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@
 
 use App\Task;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Validator;
 
 Route::get('/', function () {
     return view('tasks');
@@ -24,6 +25,21 @@ Route::get('/', function () {
  */
 Route::post('/task', function (Request $request) {
     //
+    $validator = Validator::make($request->all(), [
+        'name' => 'required|max:255',
+    ]);
+
+    if ($validator->fails()) {
+        return redirect('/')
+            ->withInput()
+            ->withErrors($validator);
+    }
+
+    $task = new Task;
+    $task->name = $request->name;
+    $task->save();
+
+    return redirect('/');
 });
 
 /**
